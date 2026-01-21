dfbccaf09032a965c72545783e891c13b64c46e0
Kauã Yanase <kauayuuki10@gmail.com>
2026-01-21 17:35:09 -0300

    cria uma tipagem para as categorias e as trasações

---
+++ src/types/category.ts
@@ -0,0 +1,9 @@
+import { LucideIcon } from "lucide-react"
+
+export type Category = {
+  id: string
+  name: string
+  color: string
+  background: string
+  icon: LucideIcon
+} 
---
+++ src/types/transaction.ts
@@ -0,0 +1,12 @@
+export type Expense = {
+  value: number
+  category: string
+  description?: string
+  valueType: string
+}
+
+export type Gain = {
+  value: number
+  description?: string
+  valueType: string
+}
