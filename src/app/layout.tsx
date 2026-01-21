db35e6117f9c4d1f5351e92890dab8d7867e2800
Kauã Yanase <kauayuuki10@gmail.com>
2026-01-21 17:43:17 -0300

    estrutura da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { TransactionProvider } from "@/context/TransactionContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="pt-br">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100`}>
+        <TransactionProvider>
+          <Header />
+          <div className="max-w-7xl mx-auto p-4">  
+            {children}
+          </div>
+        </TransactionProvider>
       </body>
     </html>
   );
