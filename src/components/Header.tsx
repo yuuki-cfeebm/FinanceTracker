b5d78ba8cd390cea124b90832411f12c37ccd97c
Kauã Yanase <kauayuuki10@gmail.com>
2026-01-22 16:01:02 -0300

    Adiciona nova rota/link para Histórico de Transações

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 export default function Header() {
   return(
-    <header className="w-full ">
-      <div className="max-w-7xl mx-auto flex p-4">  
+    <header className="w-full bg-white shadow-md shadow-blue-100">
+      <div className="max-w-7xl mx-auto flex py-6 px-2 text-xl ">  
         <div className="">FinanceTracker</div>
         <nav className="w-full flex items-center justify-center ">
-          <ul className="flex gap-12 items-center ">
+          <ul className="flex gap-12 items-center text-gray-500">
             <li className="">
-              <Link href={"/gastos"}>Registrar</Link>
+              <Link href={"/gastos"}>Registrar Transação</Link>
             </li>
             <li>
-              outro
+              <Link href={"/historico"}>Histórico de Transações</Link>
             </li>
             <li>
               outro
