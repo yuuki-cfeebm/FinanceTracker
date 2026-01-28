f634d3eb3617d2265164aca1c55076998f25b43d
Kauã Yanase <kauayuuki10@gmail.com>
2026-01-28 16:00:00 -0300

    adiciona componentes pelo shadcn

--- src/components/ui/FilterButton.tsx
+++ src/components/ui/FilterButton.tsx
@@ -3,11 +3,12 @@ import { ReactNode } from "react";
 interface FilterButtonProps {
   children: ReactNode
   onClick: () => void
+  className?: string
 }
 
-export default function FilterButton( { children, onClick }: FilterButtonProps) {
+export default function FilterButton( { children, onClick, className }: FilterButtonProps) {
   return(
-    <button className="flex items-center gap-1 shadow bg-white p-2 rounded-md border-slate-300 hover:bg-slate-100 transition cursor-pointer text-gray-600" onClick={onClick}>
+    <button className={`flex items-center gap-1 shadow bg-white p-2 rounded-md border-slate-300 hover:bg-slate-100 transition cursor-pointer text-gray-600 ${className}`} onClick={onClick}>
       {children}
     </button>
   )
