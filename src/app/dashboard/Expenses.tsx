dc4e8eaf6ad62408f85739f9a30210be86fbc907
yuuki-cfeebm <kauayuuki10@gmail.com>
2026-02-23 17:47:16 -0300

    move dateFilter para dashborard

--- src/app/dashboard/Expenses.tsx
+++ src/app/dashboard/Expenses.tsx
@@ -3,9 +3,6 @@ import { DateFilter } from "../filtros/DateFilter";
 export default function Expenses() {
   return(
     <div className="flex flex-col gap-6 w-full h-full">
-      <div className="">
-        <DateFilter />
-      </div>
       <div className="flex flex-col gap-2">
         <span className="text-gray-400">Valor total gasto:</span>
         <span className="text-4xl text-red-500">R$: 1010101</span>
